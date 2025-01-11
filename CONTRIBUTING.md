8eff8674b1d8e49240f3ef6a168c8773bdfe6d58
ayodejibot <168199873+ayodejibot@users.noreply.github.com>
2025-01-11 13:59:16 -0800

    Add files via upload

---
+++ CONTRIBUTING.md
@@ -0,0 +1,10 @@
+### How To Contribute To Us
+1. Create an Issue on Github 
+2. Add you phone number or email and tell us to Contact You
+3. Then Us how and why you want to contribute to this project
+-----------------------------------------------------------------
+
+
+#Dark-Love-MD
+
+You can send anything via Telegram t.me//darklovemd
---
+++ config.js
@@ -0,0 +1,77 @@
+//#ENJOY
+const fs = require("fs-extra");
+if (fs.existsSync(".env"))
+  require("dotenv").config({ path: __dirname + "/.env" });
+global.audio = "";
+global.video = "";
+global.port = process.env.PORT;
+global.appUrl = process.env.APP_URL || "";
+global.email = "ayodejiayanfeoluwanimi@gmail.com";
+global.location = "Njombe,Tanzania.";
+global.mongodb = process.env.MONGODB_URI || "";
+global.allowJids = process.env.ALLOW_JID || "null";
+global.blockJids = process.env.BLOCK_JID || "null";
+global.DATABASE_URL = process.env.DATABASE_URL || "";
+global.timezone = process.env.TZ || process.env.TIME_ZONE || "Africa/Dar es salaam";
+global.github = process.env.GITHUB || "https://github.com/ayodejibot/Dark-Love-MD";
+global.gurl = process.env.GURL || "https://whatsapp.com/channel/0029VagzM5RGk1Fq9b6BCO0q";
+global.website = process.env.GURL || "https://whatsapp.com/channel/0029VagzM5RGk1Fq9b6BCO0q";
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/7d7112d331a345d558149.jpg";
+global.devs = "2349071214451";
+global.sudo = process.env.SUDO || "2349071214451";
+global.owner = process.env.OWNER_NUMBER || "2349071214451";
+global.style = process.env.STYLE || "3";
+global.gdbye = process.env.GOODBYE || "true";
+global.wlcm = process.env.WELCOME || "true";
+global.warncount = process.env.WARN_COUNT || 3;
+global.disablepm = process.env.DISABLE_PM || "false";
+global.disablegroup = process.env.DISABLE_GROUPS || "false",
+global.MsgsInLog = process.env.MSGS_IN_LOG || "true";
+global.userImages = process.env.USER_IMAGES || "";
+global.waPresence = process.env.WAPRESENCE || "online";
+global.readcmds = process.env.READ_COMMAND || "false";
+global.readmessage = process.env.READ_MESSAGE || "false";
+global.readmessagefrom = process.env.READ_MESSAGE_FROM || "";
+global.read_status = process.env.AUTO_READ_STATUS || "true";
+global.save_status = process.env.AUTO_SAVE_STATUS || "false";
+global.save_status_from = process.env.SAVE_STATUS_FROM || "";
+global.read_status_from = process.env.READ_STATUS_FROM || "";
+
+global.api_smd = "https://api-smd-1.vercel.app";
+global.scan = "https://rb.gy/nl69nj";
+
+global.SESSION_ID =
+  process.env.SESSION_ID ||
+  ""
+module.exports = {
+  menu: process.env.MENU || "",
+  HANDLERS: process.env.PREFIX || ".",
+  BRANCH: process.env.BRANCH || "main",
+  VERSION: process.env.VERSION || "1.0.0",
+  caption: process.env.CAPTION || "𝘨𝘳𝘦𝘢𝘵 𝘥𝘢𝘺 | Dark-Love-MD",
+  author: process.env.PACK_AUTHER || "Dark-Love-MD",
+  packname: process.env.PACK_NAME || "♥️",
+  botname: process.env.BOT_NAME || "Dark-Love-𝐌𝐃",
+  ownername: process.env.OWNER_NAME || "Dark-Love-MD",
+  errorChat: process.env.ERROR_CHAT || "",
+  KOYEB_API: process.env.KOYEB_API || "false",
+  REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
+  OPENAI_API_KEY: process.env.OPENAI_API_KEY || "sk-proj-nzj66xdSVURMqSV8EBvpT3BlbkFJrw4a1XClnXyLUzoVftJV",
+  HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
+  HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || "",
+  antilink_values: process.env.ANTILINK_VALUES || "all",
+  HEROKU: process.env.HEROKU_APP_NAME && process.env.HEROKU_API_KEY,
+  aitts_Voice_Id: process.env.AITTS_ID || "37",
+  ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "",
+  WORKTYPE: process.env.WORKTYPE || process.env.MODE || "private",
+  LANG: (process.env.THEME || "Dark-Love-MD").toUpperCase(),
+};
+global.rank = "updated";
+global.isMongodb = false;
+let file = require.resolve(__filename);
+fs.watchFile(file, () => {
+  fs.unwatchFile(file);
+  console.log(`Update'${__filename}'`);
+  delete require.cache[file];
+  require(file);
+});
