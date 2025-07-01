0abd3da8b44ac5e395575d7950074099f01f5c2e
Ay123xx4 <167029419+Ay123xx4@users.noreply.github.com>
2025-07-01 16:06:46 -0700

    Add files via upload

---
+++ plugins/tool-trt.js
@@ -0,0 +1,40 @@
+const axios = require('axios');
+const config = require('../config')
+const {cmd , commands} = require('../command')
+const googleTTS = require('google-tts-api')
+
+cmd({
+    pattern: "trt",
+    alias: ["translate"],
+    desc: "🌍 Translate text between languages",
+    react: "⚡",
+    category: "other",
+    filename: __filename
+},
+async (conn, mek, m, { from, q, reply }) => {
+    try {
+        const args = q.split(' ');
+        if (args.length < 2) return reply("❗ Please provide a language code and text. Usage: .translate [language code] [text]");
+
+        const targetLang = args[0];
+        const textToTranslate = args.slice(1).join(' ');
+
+        const url = `https://api.mymemory.translated.net/get?q=${encodeURIComponent(textToTranslate)}&langpair=en|${targetLang}`;
+
+        const response = await axios.get(url);
+        const translation = response.data.responseData.translatedText;
+
+        const translationMessage = `> *JAWADTECHX-KHANX-TRANSLATION*
+
+> 🔤 *Original*: ${textToTranslate}
+
+> 🔠 *Translated*: ${translation}
+
+> 🌐 *Language*: ${targetLang.toUpperCase()}`;
+
+        return reply(translationMessage);
+    } catch (e) {
+        console.log(e);
+        return reply("⚠️ An error occurred data while translating the your text. Please try again later🤕");
+    }
+});
