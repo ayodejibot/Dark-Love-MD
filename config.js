81e6798cffbf1d1bac96eb9cbb9b38aacbdca671
Ay123xx4 <167029419+Ay123xx4@users.noreply.github.com>
2025-07-05 21:42:49 +0100

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
 // make true if you want auto reply on status 
-AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY Dark-Love-MD 🤍*",
+AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY Ⓓⓐⓡⓚ-Ⓛⓞⓥⓔ-ⓂⒹ🤍*",
 // set the auto reply massage on status reply  
 ANTI_DELETE: process.env.ANTI_DELETE || "true",
 // set true false for anti delete     
@@ -31,9 +31,9 @@ MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://telegra.ph/file/7d7112d33
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "Dark-Love-MD",
+BOT_NAME: process.env.BOT_NAME || "𝔻𝕒𝕣𝕜-𝕃𝕠𝕧𝕖-𝕄𝔻",
 // add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "Dark-Love-MD",
+STICKER_NAME: process.env.STICKER_NAME || "Ⓓⓐⓡⓚ-Ⓛⓞⓥⓔ-ⓂⒹ",
 // type sticker pack name 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
@@ -73,7 +73,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "923427582273",
+DEV: process.env.DEV || "2348122441244",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
