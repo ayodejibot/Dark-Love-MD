850d8ac78ed8d0a75fb832c2a18317ec224a6ef2
Ay123xx4 <167029419+Ay123xx4@users.noreply.github.com>
2025-07-06 15:01:04 +0100

    Update repo-search.js

--- plugins/repo-search.js
+++ plugins/repo-search.js
@@ -25,7 +25,7 @@ cmd({
     responseMsg += `🍴 *Forks*: ${data.forks_count}\n`;
     responseMsg += `👤 *Owner*: ${data.owner.login}\n`;
     responseMsg += `📅 *Created At*: ${new Date(data.created_at).toLocaleDateString()}\n`;
-    responseMsg += `\n> *© Powered by JawadTechX*`;
+    responseMsg += `\n> *© Powered by ⒶⓎ ⓉⒺⒸⒽ *`;
 
     await conn.sendMessage(from, { text: responseMsg }, { quoted: m });
   } catch (error) {
