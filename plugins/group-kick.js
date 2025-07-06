93bd5808ff7eca9cdd795fd5fcd56115e100f759
Ay123xx4 <167029419+Ay123xx4@users.noreply.github.com>
2025-07-06 16:02:19 +0100

    Update group-kick.js

--- plugins/group-kick.js
+++ plugins/group-kick.js
@@ -36,7 +36,7 @@ async (conn, mek, m, {
 
     try {
         await conn.groupParticipantsUpdate(from, [jid], "remove");
-        reply(`✅ Successfully removed this baka @${number}`, { mentions: [jid] });
+        reply(`✅ Successfully removed this BAKA (Idiot) @${number}`, { mentions: [jid] });
     } catch (error) {
         console.error("Remove command error:", error);
         reply("❌ Failed to remove the member.");
