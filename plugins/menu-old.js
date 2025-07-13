327c9bc02202fff5743cf123ecd9e034406d415a
Ay123xx4 <167029419+Ay123xx4@users.noreply.github.com>
2025-07-13 13:49:45 +0100

    Update menu-old.js

--- plugins/menu-old.js
+++ plugins/menu-old.js
@@ -123,9 +123,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 ┃◈┃• typography
 ┃◈┃• birthday
 ┃◈└───────────┈⊷
-╰──────────────┈⊷
-${config.DESCRIPTION}
-`;
+╰──────────────┈⊷`;
 
 
         await conn.sendMessage(
