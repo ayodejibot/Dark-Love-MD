aa78509c54044edf0136e7bf9f8cb90d6c48f9e9
Ay123xx4 <167029419+Ay123xx4@users.noreply.github.com>
2025-07-15 01:42:26 +0100

    Update main-updater.js

--- plugins/main-updater.js
+++ plugins/main-updater.js
@@ -16,24 +16,24 @@ cmd({
     if (!isOwner) return reply("This command is only for the bot owner.");
 
     try {
-        await reply("🔍 Checking for KHAN-MD updates...");
+        await reply("🔍 Checking for Dark-Love-MD updates...");
 
         // Fetch the latest commit hash from GitHub
-        const { data: commitData } = await axios.get("https://api.github.com/repos/JawadYT36/KHAN-MD/commits/main");
+        const { data: commitData } = await axios.get("https://api.github.com/repos/ayodejibot/Dark-Love-MD/commits/main");
         const latestCommitHash = commitData.sha;
 
         // Get the stored commit hash from the database
         const currentHash = await getCommitHash();
 
         if (latestCommitHash === currentHash) {
-            return reply("✅ Your KHAN-MD bot is already up-to-date!");
+            return reply("✅ Your Dark-Love-MD bot is already up-to-date!");
         }
 
-        await reply("🚀 Updating KHAN-MD Bot...");
+        await reply("🚀 Updating Dark-Love-MD Bot...");
 
         // Download the latest code
         const zipPath = path.join(__dirname, "latest.zip");
-        const { data: zipData } = await axios.get("https://github.com/JawadYT36/KHAN-MD/archive/main.zip", { responseType: "arraybuffer" });
+        const { data: zipData } = await axios.get("https://github.com/ayodejibot/Dark-Love-MD/archive/main.zip", { responseType: "arraybuffer" });
         fs.writeFileSync(zipPath, zipData);
 
         // Extract ZIP file
