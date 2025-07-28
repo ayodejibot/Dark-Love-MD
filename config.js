5db8930dcc9c9877639c8511b159ae7eee0a8e78
Ay123xx4 <167029419+Ay123xx4@users.noreply.github.com>
2025-07-28 10:00:39 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "IK~oKEWWSqZ#blHaK_HCaoXPvnPlb9UOz3LiU-OMGYdnkyllMFqOj5M",
+SESSION_ID: process.env.SESSION_ID || "IK~sXl2gIBB#bUVkEKU4GUkMLY1he4R2OfGTCmi3l0bPlSaXqfhnSjo",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -41,7 +41,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "2349162115028",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "2348122441244",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "AY TECH",
 // add bot owner name
